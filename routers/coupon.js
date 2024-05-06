710eb7a98a8a85bdadbcae60ad0872a8882e9b90
Anas Ahmed <anassabdalazim@gmail.com>
2024-05-06 16:59:25 +0300

    CouponR

---
+++ routers/coupon.js
@@ -0,0 +1,42 @@
+const { Coupon } = require("../models/coupon");
+const express = require("express");
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+  try {
+    const couponlist = await Coupon.find();
+    res.status(200).send(couponlist);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const coupon = await Coupon.findById(req.params.id);
+    if (!coupon) {
+      return res.status(404).json({ message: "Coupon not found" });
+    }
+    res.status(200).send(coupon);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    const coupon = new Coupon({
+      name: req.body.name,
+      expiry: req.body.expiry,
+      discount: req.body.discount,
+    });
+    const savedCoupon = await coupon.save();
+    res.status(201).send(savedCoupon);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
+
+module.exports = router;
